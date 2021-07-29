7e8ed1f9168ca6b6fcd95fede2202f23ea13de1c
Vity <fantom.box@inbox.ru>
2021-07-29 20:42:49 +0300

    feat: Создание 2-х таблиц для постов

---
+++ console/migrations/m210729_172031_create_post_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%post}}`.
+ */
+class m210729_172031_create_post_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%post}}', [
+            'id' => $this->primaryKey(),
+            'body' => $this->text(),
+            'photo' => $this->string(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%post}}');
+    }
+}
