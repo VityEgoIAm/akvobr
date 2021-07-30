013388e58e594fc75754e454a61f4fd7bc381cef
Vity <fantom.box@inbox.ru>
2021-07-30 14:05:51 +0300

    fix: Доступ только для авторизованных пользователей к добавдению новости

--- backend/controllers/PostController.php
+++ backend/controllers/PostController.php
@@ -10,6 +10,7 @@ use backend\models\TagForm;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\web\UploadedFile;
+use yii\filters\AccessControl;
 use yii\filters\VerbFilter;
 
 /**
@@ -23,6 +24,15 @@ class PostController extends Controller
     public function behaviors()
     {
         return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
